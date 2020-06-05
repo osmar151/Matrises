77cdf7af49c24bf9a45948e493a09ec38bc57ca4
osmar151 <62614167+osmar151@users.noreply.github.com>
2020-06-04 18:15:22 -0600

    REPOSITORIO TERMINADO

---
+++ Matrices/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrices
+{
+    class Program
+    {
+        static int val = 3;
+
+        static string[,] palabras = new string[val, val];
+
+        static void Main(string[] args)
+
+        {
+            
+            
+
+            for (int x = 0; x < val; x++)
+
+            {
+
+                for (int y = 0; y < val; y++)
+
+                {
+
+                
+                    Console.Write("Ingrese una palabra: ");
+                    palabras[y, x] = Console.ReadLine();
+
+                }
+
+            }
+
+            Console.WriteLine("\n----------------------------------\n");
+
+            for (int x = 0; x < val; x++)
+
+            {
+
+                for (int y = 0; y < val; y++)
+
+                {
+
+                    Console.WriteLine("palabra: " + palabras[y, x]);
+
+
+
+                }
+
+            }
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
